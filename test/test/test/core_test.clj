8d9c666a3774ea42ab91a9819e1202028c9a08d3
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-03-09 14:18:29 +1100

    Define test for all language directory

--- test/test/test/core_test.clj
+++ test/test/test/core_test.clj
@@ -4,12 +4,17 @@
             [clojure.java.shell :refer [with-sh-dir sh]]))
 
 (defn exec-test [dirpath sh-cmd]
-  (with-sh-dir (str "../topics/" dirpath) (sh "bash" "-c" sh-cmd)))
+  (:out (with-sh-dir dirpath (sh "bash" "-c" sh-cmd))))
 
-;(defn list-files [dirpath]
-;  (map #(str dirpath "/" %) (seq (.list (clojure.java.io/file dirpath)))))
-;(println (list-files "../topics/hello-world"))
+(defn list-files [dirpath]
+  (map #(str dirpath "/" %) (seq (.list (clojure.java.io/file dirpath)))))
 
-(deftest hello-world
-  (testing "Hello World - sample"
-    (is (= "Hello World!\n" (:out (exec-test "hello-world/sample" "bash run.sh"))))))
+(defn def-sh-test [basename cmd expected dirpath]
+  `(deftest ~(gensym basename)
+     (testing "dummy name"
+       (is (= ~expected (exec-test ~dirpath ~cmd))))))
+
+(defmacro def-test-for-all [basename cmd expected files]
+  `(do ~@(map #(def-sh-test basename cmd expected %) (eval files))))
+
+(def-test-for-all hello-world "bash run.sh" "Hello World!\n" (list-files "../topics/hello-world"))
