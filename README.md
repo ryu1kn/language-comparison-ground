102e4e978755e6a92212c4e2c48bc46fc10ae171
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-03-21 16:36:24 +1100

    Finished converting test runner from bash to clojure

--- README.md
+++ README.md
@@ -21,8 +21,4 @@ For example, to write code that does Hello World:
 
 1. Create a directory under `topics/hello-world`, for example `javascript`.
 1. Provide `run.sh` under the directory created in the previous step.
-1. Run tests by
-
-    ```sh
-    test/main.sh
-    ```
+1. To run all the tests, check [test/README.md](test/README.md)
--- test/test/test/core_test.clj
+++ test/test/test/core_test.clj
@@ -2,7 +2,10 @@
   (:require [clojure.test :refer :all]
             [test.core :refer :all]
             [clojure.java.shell :refer [with-sh-dir sh]]
+            [clojure.java.io :as io]
             [clojure.string :as str]))
+
+(def proj-root-path (System/getProperty "user.dir"))
 
 (defn exec-test [dirpath sh-cmd]
   (:out (with-sh-dir dirpath (sh "bash" "-c" sh-cmd))))
@@ -12,7 +15,17 @@
 
 (defn lang-name [dirpath] (last (str/split dirpath #"/")))
 
+(io/make-parents (str proj-root-path "tmp/whatever"))
+
 (deftest hello-world
   (doseq [file (list-files "../topics/hello-world")]
     (testing (lang-name file)
       (is (= "Hello World!\n" (exec-test file "bash run.sh"))))))
+
+(deftest file-io
+  (doseq [file (list-files "../topics/file-io")
+          out-file [(str proj-root-path "/tmp/file-io-output-" (lang-name file) ".txt")]]
+    (testing (lang-name file)
+      (is (= "Hello World!\n" (do
+        (exec-test file (str "bash run.sh" " " (.getAbsolutePath (io/file "file-io-input.txt")) " " out-file))
+        (slurp out-file)))))))
