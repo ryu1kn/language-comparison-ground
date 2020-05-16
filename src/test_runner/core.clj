0d84f3589a64d3c8789052de890ba2045c5b5900
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2020-05-16 19:10:19 +1000

    Build up a command for one solution

--- src/test_runner/core.clj
+++ src/test_runner/core.clj
@@ -3,18 +3,26 @@
             [clojure.string :as s])
   (:gen-class))
 
+(defn dirname [path] (-> path (io/file) (.getParent)))
+(defn basename [path] (-> path (io/file) (.getName)))
+
 (defn list-files [dirpath]
   (map #(str dirpath "/" %) (seq (.list (io/file dirpath)))))
 
 (defn target-dirs [dirpath]
   (mapcat list-files (list-files dirpath)))
 
-(defn test-dir [lang-dir]
-  (-> lang-dir (s/split #"/") (drop-last) (concat ["_test"]) (#(s/join "/" %))))
+(defn test-dir [lang-dir] (-> lang-dir (dirname) (str "/_test")))
+
+(defn run-test [target-dir]
+  (let [dir1 (test-dir target-dir)
+        dir2 (basename target-dir)]
+    ["bash" "test.sh" dir2 :dir dir1]))
 
 (defn test-all [problem-dir]
   (fn [sh]
-    (let [command ["bash" "test.sh" "clojure" :dir "problems/hello-world/_test"]]
+    (let [lang-dirs (target-dirs problem-dir)
+          command ["bash" "test.sh" "clojure" :dir "problems/hello-world/_test"]]
       (apply sh command))))
 
 (defn -main [& args] (println "Hello, World!"))
--- test/test_runner/core_test.clj
+++ test/test_runner/core_test.clj
@@ -9,8 +9,9 @@
   (testing "Find language directories"
     (is (= '("problems/hello-world/bash") (target-dirs problem-dir))))
 
-  (testing "Find test runner directories"
-    (is (= "problems/hello-world/_test" (test-dir "problems/hello-world/bash"))))
+  (testing "Invoke a test with the language directory name"
+    (is (= ["bash" "test.sh" "clojure" :dir "problems/hello-world/_test"]
+           (run-test "problems/hello-world/clojure"))))
 
   (testing "Run a test against a language directory found"
     (let [commands ((test-all problem-dir) fake-sh)]
